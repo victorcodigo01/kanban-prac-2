b3fdd2a2132a4ba38abb17be612fa0ede8cd9062
Bryan Paucarima Franco <>
2022-05-30 21:35:49 +0200

    añadimos boton de borrar y guadar en localstorage

--- src/context/tasks.provider.js
+++ src/context/tasks.provider.js
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TasksContext } from "./tasks.context";
+
+
+const STORAGE_KEY = 'tasks'
 
 export function TasksProvider({children}) {
 
-    const [tasks,setTask] = useState([])
+    const [tasks,setTask] = useState(() => {
+        const stored = localStorage.getItem(STORAGE_KEY);
+        return stored ? JSON.parse(stored) : []
+    })
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY,JSON.stringify(tasks))
+    },[tasks])
 
 
     const onCreateTasks = (e, status) => {
@@ -13,13 +23,16 @@ export function TasksProvider({children}) {
             status: status,
             date: new Date().toLocaleString()
         }
-
         setTask((prevTasks) => [...prevTasks,task])
-        
+    }
+
+    const onDeleteTasks = (id) => {
+        setTask((prevTasks) => prevTasks.filter(task => task.id !== id) )
     }
 
     return <TasksContext.Provider value={{
             onCreateTasks,
+            onDeleteTasks,
             tasks,
         }}>{children}</TasksContext.Provider>
 
